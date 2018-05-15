b205460842f3f94ced1fce6bd964e0d1fab8d168
Robert <fishwickrob@live.com>
2018-05-15 12:50:22 +0100

    Airport land method

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -3,8 +3,10 @@ describe('Airport', function(){
   var airport;
   var DEFAULT_CAPACITY = 3;
 
+
   beforeEach(function(){
     airport = new Airport();
+    plane = jasmine.createSpyObj('plane', ['takeOff', 'land']);
   });
 
   describe('#initialize', function(){
@@ -16,6 +18,16 @@ describe('Airport', function(){
     it('has default capacity of 3', function(){
       expect(airport.capacity).toEqual(DEFAULT_CAPACITY);
     });
+
+  });
+
+  describe('#land', function(){
+    it('lands and stores a plane', function(){
+      airport.land(plane);
+      expect(airport.planes).toContain(plane);
+
+    });
+
 
   });
 
--- src/Airport.js
+++ src/Airport.js
@@ -4,4 +4,10 @@ function Airport(){
 
   this.planes = [];
   this.capacity = DEFAULT_CAPACITY;
-}
+};
+
+Airport.prototype.land = function(plane){
+  this.planes.push(plane);
+  return this.planes;
+
+};
